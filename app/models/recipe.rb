72e76172c6cbe93e787d29f8e4c3e68d2ffd28d5
saluumaa <salumahassan02@gmail.com>
2023-09-05 19:48:44 +0300

    Update food and user model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,9 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
-end
+
+
+  attribute :public, :boolean, default: false
+
+
+end  
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :recipes, dependent: :destroy
+  has_many :foods, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :foods
-
+  
   validates :name, presence: true
 end
