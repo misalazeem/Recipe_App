7d9982361008902b92aa59832d3a768a48555de2
saluumaa <salumahassan02@gmail.com>
2023-09-05 16:04:01 +0300

    Fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,9 +1,7 @@
 class Food < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_foods
+  has_many :recipes, through: :recipe_foods
 
-    belongs_to :user
-    has_many :recipe_foods
-    has_many :recipes, through: :recipe_foods
-  
-    validates :name, presence: true
-
+  validates :name, presence: true
 end
