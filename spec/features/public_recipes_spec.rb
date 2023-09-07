1435f2f4ed9497a2bea4d118806f5836dde30102
saluumaa <salumahassan02@gmail.com>
2023-09-07 10:16:42 +0300

    Fix linter errors

--- spec/features/public_recipes_spec.rb
+++ spec/features/public_recipes_spec.rb
@@ -9,11 +9,9 @@ RSpec.feature 'Public Recipes', type: :feature do
 
     visit new_user_session_path
 
-
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'password'
     click_button 'Log in'
-
 
     visit public_recipes_recipes_path
     expect(page).to have_content('Public Recipes')
