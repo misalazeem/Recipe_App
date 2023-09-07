05e6f950737f2ee03fbf61eb8d17dd6eda70f3af
saluumaa <salumahassan02@gmail.com>
2023-09-07 10:05:33 +0300

    Add test for public recipes page

---
+++ spec/features/public_recipes_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature 'Public Recipes', type: :feature do
+  scenario 'User views public recipes' do
+
+    user = User.create(name: 'John', email: 'john@example.com', password: 'password')
+
+    recipe1 = Recipe.create(name: 'Sample Recipe 1', user: user)
+    recipe2 = Recipe.create(name: 'Sample Recipe 2', user: user)
+
+    visit new_user_session_path
+
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+
+
+    visit public_recipes_recipes_path
+    expect(page).to have_content('Public Recipes')
+    
+  end
+end
